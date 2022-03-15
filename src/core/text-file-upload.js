37a972d59ca2356eb5e1fededfe3959b8654f2e3
michael-lugashi <michael.lugashi@gmail.com>
2022-03-15 17:46:07 +0200

    removed comments

--- src/core/text-file-upload.js
+++ src/core/text-file-upload.js
@@ -1,24 +1,45 @@
 import React from 'react';
 
 function TextFileUpload(props) {
-    const [text, setText] = React.useState('');
-    return (
-        <div>
-            <input type='file' onChange={async (e)=>{
-                const file = e.target.files[0];
-                const fileText= await file.text();
-                setText(fileText)
-                // console.log(fileText);
-                for (let i = 0; i < fileText.length; i++) {
-                    if (fileText[i] === '\n') {
-                        console.log('found one!!!!!')
-                    }
-                   console.log(fileText[i]);
-                    }
-                }}/>
-          
-            <div>{text}</div>
-        </div>
-    );
+  const [text, setText] = React.useState('');
+
+  const removeComments = (fileText) => {
+    let removedCommentsText = '';
+    let singleLineSkip = false;
+    let multiLineSkip = false;
+    for (let i = 0; i < fileText.length; i++) {
+      if (fileText[i] === '/' && fileText[i + 1] === '/') {
+        singleLineSkip = true;
+      } else if (fileText[i] === '\n' && singleLineSkip === true) {
+        singleLineSkip = false;
+      } else if (fileText[i] === '/' && fileText[i + 1] === '*') {
+        multiLineSkip = true;
+      } else if (
+        fileText[i - 1] === '*' &&
+        fileText[i] === '/' &&
+        multiLineSkip === true
+      ) {
+        multiLineSkip = false;
+      } else if (singleLineSkip === false && multiLineSkip === false) {
+        removedCommentsText += fileText[i];
+      }
+    }
+    return removedCommentsText;
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={async (e) => {
+          const file = e.target.files[0];
+          const fileText = await file.text();
+          setText(removeComments(fileText));
+        }}
+      />
+
+      <div>{text}</div>
+    </div>
+  );
 }
 export default TextFileUpload;
