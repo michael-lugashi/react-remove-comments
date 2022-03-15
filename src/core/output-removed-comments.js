7cd2efc18ddf24f07f5ef83a48b051a9d71c6d53
michael-lugashi <michael.lugashi@gmail.com>
2022-03-15 18:33:12 +0200

    added buttons to change routes

--- src/core/output-removed-comments.js
+++ src/core/output-removed-comments.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function OutputRemovedComments({ removedCommentsText }) {
-//   console.log(props);
-  return <div>output: {removedCommentsText}</div>;
+  //   console.log(props);
+  return (
+    <div>
+      output: {removedCommentsText}
+      <Link
+        to={{
+          pathname: '/read-file',
+        }}
+      >
+        <button>upload file</button>
+      </Link>
+    </div>
+  );
 }
 
 export default OutputRemovedComments;
